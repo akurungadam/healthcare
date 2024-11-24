4e9c6561ee1934afeae75ee12fb064de1c3b9fd1
Anoop Kurungadam <anoop@earthianslive.com>
2024-11-25 00:03:11 +0530

    feat: add frappe-ui to practitioner wroklist page

--- healthcare/healthcare/page/practitioner_worklist/practitioner_worklist.js
+++ healthcare/healthcare/page/practitioner_worklist/practitioner_worklist.js
@@ -1,7 +1,23 @@
-frappe.pages['practitioner-worklist'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
+frappe.pages["practitioner-worklist"].on_page_load = function (wrapper) {
+	frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Practitioner Worklist',
-		single_column: true
+		title: __("Practitioner Worklist"),
+		single_column: true,
+	});
+};
+
+frappe.pages["practitioner-worklist"].on_page_show = function (wrapper) {
+	load_desk_page(wrapper);
+};
+
+function load_desk_page(wrapper) {
+	let $parent = $(wrapper).find(".layout-main-section");
+	$parent.empty();
+
+	frappe.require("practitioner_worklist.bundle.js").then(() => {
+		frappe.practitioner_worklist = new frappe.ui.PractitionerWorklist({
+			wrapper: $parent,
+			page: wrapper.page,
+		});
 	});
 }
---
+++ healthcare/public/js/practitioner_worklist/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const dynamicMessage = ref("Hello from App.vue");
+</script>
+<template>
+  <div>
+	<h3>{{ dynamicMessage }}</h3>
+    <h4>Start editing at healthcare/public/js/practitioner_worklist/App.vue</h4>
+  </div>
+</template>
